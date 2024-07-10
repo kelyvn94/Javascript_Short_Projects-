a097aa0a70dcf7d0b97d5a3bd562b4d3dcf6297d
kelvyn94 <kmacharia584@gmail.com>
2024-07-10 14:43:29 +0300

    finishing the slider project on function js

--- Slide Bar/slider.js
+++ Slide Bar/slider.js
@@ -1,8 +1,27 @@
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
-const slider = document.querySelector(".slider-frame");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
+
+
+let sliderNumber = 1; 
+
+const length = images.length;
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${sliderNumber * 800}px)`;
+    sliderNumber++;
+}
+
+const previousSlide = () => {
+    slider.style.transform = `translateX(0px)`;
+    sliderNumber = 1;
+}
 
 
 right.addEventListener("click", ()=> {
-    slider.style.transform = `translateX(-800px)`
-})
+
+    (sliderNumber < length) ? nextSlide() : previousSlide();
+    
+});
+    
