313fb5daf7c243c7b1110fe4c758f03df111a355
kelvyn94 <kmacharia584@gmail.com>
2024-07-13 22:29:42 +0300

    adding js codes

--- Slide Bar/slider.js
+++ Slide Bar/slider.js
@@ -39,3 +39,31 @@ left.addEventListener("click", ()=> {
     (sliderNumber > 1) ? previousSlide() : nextSlide();
 
 });
+
+
+
+
+//Correction
+
+
+const getFirstSlide = () => {
+    slider.style.transform = `translateX(0px)`;
+    sliderNumber = 1;
+}
+
+const getLastSlide = () => {
+    slider.style.transform = `translateX(${(length - 1) * 800}px)`;
+    sliderNumber = 1;
+}
+
+right.addEventListener("click", ()=> {
+
+    (sliderNumber < length) ? nextSlide() : previousSlide();
+    
+});
+    
+left.addEventListener("click", ()=> {
+
+    (sliderNumber > 1) ? previousSlide() : nextSlide();
+
+});
